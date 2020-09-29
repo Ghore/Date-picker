c6f56bedeb487ae34fc1e86cbaf21151f4197bed
Ghore <ghoreua@gmail.com>
2020-09-29 09:10:42 +0300

    Updates

---
+++ precache-manifest.1701e801224ff3b1b566f9cfe0fe28a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393aeae77254c9e7e3dd7b7a83cb02c2",
+    "url": "/date-picker/index.html"
+  },
+  {
+    "revision": "1bc34bb0dc3fec3bd12b",
+    "url": "/date-picker/static/css/main.f8b133f1.chunk.css"
+  },
+  {
+    "revision": "0470884fe4fe32aedd61",
+    "url": "/date-picker/static/js/2.056e0171.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/date-picker/static/js/2.056e0171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc34bb0dc3fec3bd12b",
+    "url": "/date-picker/static/js/main.7ad17554.chunk.js"
+  },
+  {
+    "revision": "a46d4942d6816cf0e8dd",
+    "url": "/date-picker/static/js/runtime-main.33e59597.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/date-picker/static/media/Background.37a75b54.jpg"
+  },
+  {
+    "revision": "64a8478366d65e2d2dc054833fc124db",
+    "url": "/date-picker/static/media/Logo original RGB new.64a84783.svg"
+  }
+]);
